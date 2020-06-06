4c1697eb75b5d48d507e9b7c81512c736f1e50cf
dz2701 <shin102766@gmail.com>
2020-06-06 18:55:54 +0900

    dddd

---
+++ main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <span>
+      <button>{{message}}</button>
+    </span>
+  </div>
+</template>
+
+<script>
+export default{
+  data:new Promise(function(resolve, reject) {
+    message: "click this button!"
+  });
+}
+</script>
+
+<style>
+span {
+  font-size:1.2em;
+}
+</style>
